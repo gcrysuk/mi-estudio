12f76438737961098ccee95d0c854289f2a07ea1
Gabriel Crysuk <gabrielcrysuk@gmail.com>
2026-06-09 21:01:12 +0000

    fix: merge migraciones usuarios 0004 conflicto prod

---
+++ backend/apps/usuarios/migrations/0005_merge_0004.py
@@ -0,0 +1,8 @@
+from django.db import migrations
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('usuarios', '0004_perfilusuario_notificacion_config'),
+        ('usuarios', '0004_alter_perfilusuario_fecha_registro'),
+    ]
+    operations = []
